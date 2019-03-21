b157a95b88d4cc0c3ce532e1bfe207b38a1ab056
Volodymyr Bohutskyi <volodymyr.bohutskyi@materialise.kiev.ua>
2019-03-21 15:52:57 +0200

    initial commit

---
+++ WebApi/WebApi/Models/PaymentDetailContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+    }
+}
